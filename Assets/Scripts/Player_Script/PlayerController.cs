776916fc65a038ce68bf70e3b7223c61ebf9a1ef
Highwall-27 <mauropio.altamura@outlook.it>
2022-05-10 17:53:11 +0200

    Added collisions

--- Assets/Scripts/Player_Script/PlayerController.cs
+++ Assets/Scripts/Player_Script/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public LayerMask solidObjectsLayer;
 
     private bool isMoving;
     private Vector2 input;
@@ -33,8 +34,11 @@ public class PlayerController : MonoBehaviour
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
-
-                StartCoroutine(Move(targetPos));
+                if (IsWalkable(targetPos))
+                {
+                    StartCoroutine(Move(targetPos));
+                }
+                
             }
         }
 
@@ -56,4 +60,14 @@ public class PlayerController : MonoBehaviour
 
         isMoving = false;
     }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if(Physics2D.OverlapCircle(targetPos, 0.1f, solidObjectsLayer) != null)
+        {
+            return false;
+        }
+        return true;
+    }
+
 }
