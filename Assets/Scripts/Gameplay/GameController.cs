adf20a96c3a421ed8b0f770f30417f73b0897af5
Highwall-27 <mauropio.altamura@outlook.it>
2022-05-19 10:51:04 +0200

    Implemented additive scene switching

--- Assets/Scripts/Gameplay/GameController.cs
+++ Assets/Scripts/Gameplay/GameController.cs
@@ -12,6 +12,10 @@ public class GameController : MonoBehaviour
     GameState state;
     GameState stateBeforePause;
 
+    public SceneDetails CurrentScene { get; private set; }
+    public SceneDetails PrevScene { get; private set; }
+
+
     public static GameController Instance { get; private set; }
 
     private void Awake()
@@ -58,5 +62,11 @@ public class GameController : MonoBehaviour
         {
             DialogManager.Instance.HandleUpdate();
         }
+    }
+
+    public void SetCurrentScene(SceneDetails currScene)
+    {
+        PrevScene = CurrentScene;
+        CurrentScene = currScene;
     }
 }
---
+++ Assets/Scripts/SceneManagement/SceneDetails.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneDetails : MonoBehaviour
+{
+    [SerializeField] List<SceneDetails> connectedScenes;
+
+    public bool isLoaded { get; private set; }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log($"Entered {gameObject.name}");
+
+            LoadScene();
+            GameController.Instance.SetCurrentScene(this);
+            foreach (var scene in connectedScenes)
+            {
+                scene.LoadScene();
+            }
+
+            if(GameController.Instance.PrevScene != null)
+            {
+                var previouslyLoadedScenes = GameController.Instance.PrevScene.connectedScenes;
+                foreach (var scene in previouslyLoadedScenes)
+                {
+                    if (!connectedScenes.Contains(scene) && scene != this)
+                    {
+                        scene.UnloadScene();
+                    }
+                }
+            }
+        }
+    }
+
+    public void LoadScene()
+    {
+        if (!isLoaded)
+        {
+            SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
+            isLoaded = true;
+        }
+    }
+
+    public void UnloadScene()
+    {
+        if (isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(gameObject.name);
+            isLoaded = false;
+        }
+    }
+}
